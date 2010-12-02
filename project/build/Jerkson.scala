07e161633bf92c60d25cc548884dda8f46c2473c
Coda Hale <coda.hale@gmail.com>
2010-12-02 14:44:54 -0800

    Start using simplespec 0.2.0.

--- project/build/Jerkson.scala
+++ project/build/Jerkson.scala
@@ -36,6 +36,6 @@ class Jerkson(info: ProjectInfo) extends DefaultProject(info)
    * Test Dependencies
    */
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test" withSources ()
-  val simplespec = "com.codahale" % "simplespec_2.8.0" % "0.2.0-SNAPSHOT" % "test" withSources ()
+  val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test" withSources ()
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources ()
 }
