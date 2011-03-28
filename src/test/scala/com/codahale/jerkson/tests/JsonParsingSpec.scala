7069e58283d17b96d76f632a38e0d6e51c60ced4
Coda Hale <coda.hale@gmail.com>
2011-03-28 16:15:47 -0700

    Start parsing the Scala sig of case classes.
    Scala 2.9.x treats covariant type parameters as raw types in the byte code,
    which means a case class with Map[String, Long] gets treated by standard
    Java reflection (i.e., the only reflection we have) as Map[String, Object]
    which of course means all sorts of weird shit breaks loose.
    
    This change includes a repackaged, non-compiler-dependent version of scalap
    and some heavily-altered code from Novus' Salat library. (h/t Jorge Ortiz
    for the reference)

--- src/test/scala/com/codahale/jerkson/tests/JsonParsingSpec.scala
+++ src/test/scala/com/codahale/jerkson/tests/JsonParsingSpec.scala
@@ -43,8 +43,7 @@ object JsonParsingSpec extends Spec {
 
       parse[Person]("{\"woo\": 1}") must throwA[ParsingException].like {
         case e: ParsingException => {
-          e.getMessage must beEqualTo("Invalid JSON. Needed either [id, name] " +
-                                              "or [id, firstName, lastName], " +
+          e.getMessage must beEqualTo("Invalid JSON. Needed [id, name], " +
                                               "but found [woo].")
         }
       }
@@ -217,12 +216,6 @@ object JsonParsingSpec extends Spec {
   }
 
   class `Parsing a JSON object as a case class` {
-    def `should use all available constructors` {
-      parse[Person](""" {"id":1, "name": "Coda"} """) must beEqualTo(Person(1, "Coda"))
-
-      parse[Person](""" {"id":1, "firstName": "Coda", "lastName": "Hale"} """) must beEqualTo(Person(1, "Coda Hale"))
-    }
-
     def `should handle missing Option members` {
       parse[ClassWithOption](""" {"one": "1"} """) must beEqualTo(ClassWithOption("1", None))
 
@@ -292,12 +285,18 @@ object JsonParsingSpec extends Spec {
       parse[Either[Int, String]]("\"woo\"") must beEqualTo(Right("woo"))
     }
   }
+
+  class `Parsing a JSON object as a case class with Map arguments` {
+    def `should use the Scala signature to detect map value types` {
+      parse[ClassWithMap]("""{"properties": {"yay": 400}}""") must beEqualTo(ClassWithMap(Map("yay" -> 400)))
+    }
+  }
 }
 
-case class Person(id: Long, name: String) {
-  def this(id: Long, firstName: String, lastName: String) = this(id, firstName + " " + lastName)
-}
+case class Person(id: Long, name: String)
 
 case class ClassWithOption(one: String, two: Option[String])
 
 case class ClassWithJsonNode(one: String, two: JsonNode)
+
+case class ClassWithMap(properties: Map[String, Long])
