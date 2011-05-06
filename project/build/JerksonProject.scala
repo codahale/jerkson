078432a2873d03e9f71fa6823e19c62f5350738a
Coda Hale <coda.hale@gmail.com>
2011-05-05 23:29:12 -0700

    Move to Specs2.

--- project/build/JerksonProject.scala
+++ project/build/JerksonProject.scala
@@ -30,7 +30,7 @@ class JerksonProject(info: ProjectInfo) extends DefaultProject(info)
   /**
    * Test Dependencies
    */
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
-  val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test"
-  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test"
+  val specs2 = "org.specs2" %% "specs2" % "1.2" % "test"
+  def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
+  override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
 }
--- src/test/scala/com/codahale/jerkson/tests/JValueSpec.scala
+++ src/test/scala/com/codahale/jerkson/tests/JValueSpec.scala
@@ -1,50 +1,48 @@
 package com.codahale.jerkson.tests
 
-import com.codahale._
-import simplespec._
-import jerkson._
-import Json._
-import AST._
+import org.specs2.mutable.Specification
+import com.codahale.jerkson.Json._
+import com.codahale.jerkson.AST._
 
-object JValueSpec extends Spec {
-  class `selecting single nodes` {
-    def `should return None with primitives` {
+class JValueSpec extends Specification {
+  "Selecting single nodes" should {
+    "return None with primitives" in {
       parse[JValue]("8") \ "blah" must be(JNull)
     }
     
-    def `should return None on nonexistant fields` {
+    "return None on nonexistant fields" in {
       parse[JValue]("{\"butt\": \"poop\"}") \ "anus" must be(JNull)
     }
     
-    def `should return a JValue with an existing field` {
+    "return a JValue with an existing field" in {
       parse[JValue]("{\"butt\": \"poop\"}") \ "butt" must beEqualTo(JString("poop"))
     }
   }
   
-  class `selecting array members` {
-    def `should return  None with primitives` {
+  "Selecting array members" should {
+    "return  None with primitives" in {
       parse[JValue]("\"derp\"").apply(0) must be(JNull)
     }
     
-    def `should return None on out of bounds` {
+    "return None on out of bounds" in {
       parse[JValue]("[0, 1, 2, 3]").apply(4) must be(JNull)
     }
     
-    def `should return a JValue` {
+    "return a JValue" in {
       parse[JValue]("[0, 1, 2, 3]").apply(2) must beEqualTo(JInt(2))
     }
   }
   
-  class `deep select` {
-    def `should return None with primitives` {
+  "Deep selecting" should {
+    "return None with primitives" in {
       parse[JValue]("0.234") \\ "herp" must be(Nil)
     }
     
-    def `should return None on nothing found` {
+    "return None on nothing found" in {
       parse[JValue]("{\"butt\": {\"anus\" : \"poopoo\"}}") \\ "anus" must beEqualTo(Seq(JString("poopoo")))
     }
     
-    def `should return multiple leaf nodes` {
+    "return multiple leaf nodes" in {
       parse[JValue]("{\"butt\": {\"anus\" : \"poopoo\"}, \"rectum\": {\"anus\" : \"dookie\"}}") \\ "anus" must beEqualTo(Seq(JString("poopoo"),JString("dookie")))
     }
   }
