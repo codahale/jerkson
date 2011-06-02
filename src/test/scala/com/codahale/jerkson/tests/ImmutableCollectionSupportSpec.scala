f2f0a895c18c311b63adee3d16f9c6572b24e7f9
Coda Hale <coda.hale@gmail.com>
2011-06-01 18:38:58 -0700

    Giving up for now on how to deser TreeMaps and TreeSets.

--- src/test/scala/com/codahale/jerkson/tests/ImmutableCollectionSupportSpec.scala
+++ src/test/scala/com/codahale/jerkson/tests/ImmutableCollectionSupportSpec.scala
@@ -52,6 +52,8 @@ class ImmutableCollectionSupportSpec extends Spec {
       generate(TreeSet(1)) must beEqualTo("[1]")
     }
 
+    // TODO: 6/1/11 <coda> -- figure out how to deserialize TreeSet instances
+
     /**
      * I'm not entirely sure I can do this, since I need an Ordering instance
      * before I can do this. I'd need to go from the intended element type to
@@ -60,13 +62,13 @@ class ImmutableCollectionSupportSpec extends Spec {
      */
 
     def `is parsable from a JSON array of ints` = {
-      pending // TODO: 5/31/11 <coda> -- fix immutable.TreeSet deserialization
 //      parse[TreeSet[Int]]("[1,2,3]") must beEqualTo(TreeSet(1, 2, 3))
+      pending
     }
 
     def `is parsable from an empty JSON array` = {
-      pending // TODO: 5/31/11 <coda> -- fix immutable.TreeSet deserialization
 //      parse[TreeSet[Int]]("[]") must beEqualTo(TreeSet.empty[Int])
+      pending
     }
   }
 
@@ -103,14 +105,23 @@ class ImmutableCollectionSupportSpec extends Spec {
       generate(TreeMap("one" -> 1)) must beEqualTo("""{"one":1}""")
     }
 
+    // TODO: 6/1/11 <coda> -- figure out how to deserialize TreeMap instances
+
+    /**
+     * I'm not entirely sure I can do this, since I need an Ordering instance
+     * before I can do this. I'd need to go from the intended element type to
+     * the Ordering instance, but that's done via the implicit scoping, which I
+     * can't do with just a Class[_] instance.
+     */
+
     def `is parsable from a JSON object with int field values` = {
-      pending // TODO: 5/31/11 <coda> -- fix immutable.TreeMap deserialization
 //      parse[TreeMap[String, Int]]("""{"one":1}""") must beEqualTo(TreeMap("one" -> 1))
+      pending
     }
 
     def `is parsable from an empty JSON object` = {
-      pending // TODO: 5/31/11 <coda> -- fix immutable.TreeMap deserialization
 //      parse[TreeMap[String, Int]]("{}") must beEqualTo(TreeMap.empty[String, Int])
+      pending
     }
   }
 
