d314bbe5173c7347da43bd07b15cb80c639b1086
Coda Hale <coda.hale@gmail.com>
2011-06-04 19:46:36 -0700

    Remove Parser#parseStreamOf.
    Feels good man.

--- src/test/scala/com/codahale/jerkson/tests/StreamingSpec.scala
+++ src/test/scala/com/codahale/jerkson/tests/StreamingSpec.scala
@@ -1,7 +1,6 @@
 package com.codahale.jerkson.tests
 
 import com.codahale.jerkson.Json._
-import collection.mutable.ArrayBuffer
 import java.io.ByteArrayInputStream
 import com.codahale.simplespec.Spec
 
@@ -13,20 +12,6 @@ class StreamingSpec extends Spec {
       {"id":3, "name": "Biscuit"},
       {"id":4, "name": "Louie"}
     ]"""
-
-    def `fires a callback for each stream element` = {
-      val input = new ByteArrayInputStream(json.getBytes)
-
-      val people = new ArrayBuffer[CaseClass]
-      parseStreamOf[CaseClass](input) { p =>
-        people += p
-      }
-
-      people.toSeq must beEqualTo(Seq(CaseClass(1, "Coda"),
-                                      CaseClass(2, "Niki"),
-                                      CaseClass(3, "Biscuit"),
-                                      CaseClass(4, "Louie")))
-    }
 
     def `returns an iterator of stream elements` = {
       stream[CaseClass](new ByteArrayInputStream(json.getBytes)).toList must
