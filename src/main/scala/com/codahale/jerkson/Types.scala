5539a8602bcbd9518b9b13c7f6dbbdda9071918d
Coda Hale <coda.hale@gmail.com>
2011-07-25 15:20:33 -0700

    Added a cache for types from manifests.

--- src/main/scala/com/codahale/jerkson/Types.scala
+++ src/main/scala/com/codahale/jerkson/Types.scala
@@ -2,15 +2,22 @@ package com.codahale.jerkson
 
 import org.codehaus.jackson.`type`.JavaType
 import org.codehaus.jackson.map.`type`.{TypeFactory, ArrayType}
+import scala.collection.JavaConversions.asScalaConcurrentMap
+import java.util.concurrent.ConcurrentHashMap
 
 private[jerkson] object Types {
-  def build[A](factory: TypeFactory, manifest: Manifest[A]): JavaType = {
+  private val cachedTypes = asScalaConcurrentMap(new ConcurrentHashMap[Manifest[_], JavaType]())
+
+  def build(factory: TypeFactory, manifest: Manifest[_]): JavaType =
+    cachedTypes.getOrElseUpdate(manifest, constructType(factory, manifest))
+
+  private def constructType(factory: TypeFactory, manifest: Manifest[_]): JavaType = {
     if (manifest.erasure.isArray) {
       ArrayType.construct(factory.constructType(manifest.erasure.getComponentType))
     } else {
       factory.constructParametricType(
         manifest.erasure,
-        manifest.typeArguments.map { m => build(factory, m)}.toArray: _*)
+        manifest.typeArguments.map {m => build(factory, m)}.toArray: _*)
     }
   }
 }
