7160202e2f3b944f032c51181d80f5bcf12b96d3
Joseph Walton <joe@kafsemo.org>
2012-07-28 17:07:05 +1000

    Fix tuple tests to run as part of the build.

--- src/test/scala/com/codahale/jerkson/tests/TupleSerializationSpec.scala
+++ src/test/scala/com/codahale/jerkson/tests/TupleSerializationSpec.scala
@@ -5,36 +5,48 @@ import com.codahale.jerkson.Json
 import com.codahale.jerkson.ParsingException
 import com.codahale.simplespec.Spec
 
-class TupleSerializationTest extends Spec {
-  @Test def serializedAsArray() = {
-    Json.generate((1, "two", 3.0)).must(be("[1,\"two\",3.0]"))
+class TupleSerializationSpec extends Spec {
+  class `Serialization` {
+    @Test def serializedAsArray() = {
+      Json.generate((1, "two", 3.0)).must(be("[1,\"two\",3.0]"))
+    }
+
+    @Test def serializedAsArrayWithUniformType() = {
+      Json.generate((1, 2, 3)).must(be("[1,2,3]"))
+    }
   }
 
-  @Test def parsedAsArrayWithAllIntegers() = {
-    Json.parse[(Int, Int, Int)]("[1, 2, 3]").must(be((1, 2, 3)))
+  class `Parsing from arrays` {
+    @Test def parsedAsArrayWithAllIntegers() = {
+      Json.parse[(Int, Int, Int)]("[1, 2, 3]").must(be((1, 2, 3)))
+    }
+
+    @Test def parsedAsArray() = {
+      Json.parse[(Int, String, Double)]("[1, \"two\", 3.0]").must(be((1, "two", 3.0)))
+    }
   }
 
-  @Test def parsedAsArray() = {
-    Json.parse[(Int, String, Double)]("[1, \"two\", 3.0]").must(be((1, "two", 3.0)))
+  class `Expected parsing failures` {
+    @Test(expected = classOf[ParsingException])
+    def errorWhenTooFewFieldsForTuple() = {
+      Json.parse[(Int, Int)]("[1]")
+    }
+
+    @Test(expected = classOf[ParsingException])
+    def errorWhenTooManyFieldsForTuple() = {
+      Json.parse[(Int)]("[1,2]")
+    }
   }
 
-  @Test(expected = classOf[ParsingException])
-  def errorWhenTooFewFieldsForTuple() = {
-    Json.parse[(Int, Int)]("[1]")
-  }
+  class `Parses up to largest defined tuple` {
+    @Test def longestTupleLengthIsParsedWithAllIntegers() = {
+      val numbers  = 1 to 22
+      val str = "[" + numbers.mkString(",") + "]"
 
-  @Test(expected = classOf[ParsingException])
-  def errorWhenTooManyFieldsForTuple() = {
-    Json.parse[(Int)]("[1,2]")
-  }
+      val t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
 
-  @Test def longestTupleLengthIsParsedWithAllIntegers() = {
-    val numbers  = 1 to 22
-    val str = "[" + numbers.mkString(",") + "]"
-
-    val t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
-
-    Json.parse[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)](str).must(
-        be(t))
+      Json.parse[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)](str).must(
+          be(t))
+    }
   }
 }
