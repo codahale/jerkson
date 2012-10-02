ba8efac052084b2445e06425536789b43ceacf6d
Ian Eure <ian.eure@gmail.com>
2012-10-02 10:00:05 -0700

    Fix tests.

--- src/test/scala/com/codahale/jerkson/tests/FancyTypeSupportSpec.scala
+++ src/test/scala/com/codahale/jerkson/tests/FancyTypeSupportSpec.scala
@@ -33,41 +33,42 @@ class FancyTypeSupportSpec extends Spec {
   }
 
   class `A Joda DateTime` {
-    val ts = new DateTime("1883-07-03")
+    val ts = new DateTime("1883-07-03T00:00:00.000-08:00:00")
 
     @Test def `Generates a JSON int` {
-      generate(ts).must(be("-2729606822000"))
+      generate(ts).must(be("-2729606400000"))
     }
 
     @Test def `Is parsable from a JSON string` = {
-      parse[DateTime]("-2729606822000").compareTo(ts).must(be(0))
+      parse[DateTime]("-2729606400000").compareTo(ts).must(be(0))
     }
   }
 
   class `A Joda DateTime sequence` {
-    val ts = Seq(new DateTime("1883-07-03"),
-                 new DateTime("1924-06-03"))
+    val ts = Seq(new DateTime("1883-07-03T00:00:00.000-08:00:00"),
+                 new DateTime("1924-06-03T00:00:00.000-08:00:00"))
 
     @Test def `Generates a JSON int list` {
-      generate(ts).must(be("[-2729606822000,-1438358400000]"))
+      generate(ts).must(be("[-2729606400000,-1438358400000]"))
     }
 
     @Test def `Is parsable from a JSON string` = {
-      val dates = parse[Seq[DateTime]]("[-2729606822000, -1438358400000]")
+      val dates = parse[Seq[DateTime]]("[-2729606400000, -1438358400000]")
       dates(0).compareTo(ts(0)).must(be(0))
       dates(1).compareTo(ts(1)).must(be(0))
     }
   }
 
   class `A case class with a Joda DateTime` {
-    val ent = LogEntry(new DateTime("1883-07-03"), "birth")
+    val ent = LogEntry(new DateTime("1883-07-03T00:00:00.000-08:00:00"),
+                              "birth")
 
     @Test def `Generates a JSON object` {
-      generate(ent).must(be("""{"when":-2729606822000,"message":"birth"}"""))
+      generate(ent).must(be("""{"when":-2729606400000,"message":"birth"}"""))
     }
 
     @Test def `Is parsable from a JSON object` = {
-      val parsedEnt = parse[LogEntry]("""{"when": -2729606822000, "message": "birth"}""")
+      val parsedEnt = parse[LogEntry]("""{"when": -2729606400000, "message": "birth"}""")
       parsedEnt.when.compareTo(ent.when).must(be(0))
       parsedEnt.message.must(be(ent.message))
     }
