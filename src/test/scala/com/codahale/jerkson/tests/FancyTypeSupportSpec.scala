3461c22e9e548f8188a36f71e9adf7de6bd055b6
Ian Eure <ian.eure@gmail.com>
2012-10-02 09:03:25 -0700

    Add more Joda tests.

--- src/test/scala/com/codahale/jerkson/tests/FancyTypeSupportSpec.scala
+++ src/test/scala/com/codahale/jerkson/tests/FancyTypeSupportSpec.scala
@@ -7,6 +7,8 @@ import org.joda.time.DateTime
 import com.codahale.jerkson.Json._
 import java.util.UUID
 
+case class LogEntry(when: DateTime, message: String)
+
 class FancyTypeSupportSpec extends Spec {
   class `A URI` {
     @Test def `generates a JSON string` = {
@@ -39,6 +41,35 @@ class FancyTypeSupportSpec extends Spec {
 
     @Test def `Is parsable from a JSON string` = {
       parse[DateTime]("-2729606822000").compareTo(ts).must(be(0))
+    }
+  }
+
+  class `A Joda DateTime sequence` {
+    val ts = Seq(new DateTime("1883-07-03"),
+                 new DateTime("1924-06-03"))
+
+    @Test def `Generates a JSON int list` {
+      generate(ts).must(be("[-2729606822000,-1438358400000]"))
+    }
+
+    @Test def `Is parsable from a JSON string` = {
+      val dates = parse[Seq[DateTime]]("[-2729606822000, -1438358400000]")
+      dates(0).compareTo(ts(0)).must(be(0))
+      dates(1).compareTo(ts(1)).must(be(0))
+    }
+  }
+
+  class `A case class with a Joda DateTime` {
+    val ent = LogEntry(new DateTime("1883-07-03"), "birth")
+
+    @Test def `Generates a JSON object` {
+      generate(ent).must(be("""{"when":-2729606822000,"message":"birth"}"""))
+    }
+
+    @Test def `Is parsable from a JSON object` = {
+      val parsedEnt = parse[LogEntry]("""{"when": -2729606822000, "message": "birth"}""")
+      parsedEnt.when.compareTo(ent.when).must(be(0))
+      parsedEnt.message.must(be(ent.message))
     }
   }
 }
